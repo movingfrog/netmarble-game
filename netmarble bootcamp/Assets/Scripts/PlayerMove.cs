b0efe5488ea135366f659777e8a3905faa0e5357
icetea1110 <c01042527441@gmail.com>
2024-08-20 21:12:57 +0900

    icetea

--- netmarble bootcamp/Assets/Scripts/PlayerMove.cs
+++ netmarble bootcamp/Assets/Scripts/PlayerMove.cs
@@ -12,6 +12,7 @@ public class PlayerMove : MonoBehaviour
     private float jumpTime = 0;
     private float jumpLimit = 0.25f;
     float gravity;
+
     public LayerMask groundLayer;
 
     bool IsGround;
@@ -65,7 +66,7 @@ public class PlayerMove : MonoBehaviour
         {
             gravity = rb.gravityScale;
             rb.gravityScale = 0;
-            Debug.Log("√Å¬°√á√Å");
+            Debug.Log("¡°«¡");
             rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
             IsJumping = true;
             IsJump = true;
@@ -73,7 +74,7 @@ public class PlayerMove : MonoBehaviour
         }
         if (IsJumping && Input.GetKey(KeyCode.C))
         {
-            Debug.Log("√Å¬°√á√Å√Å√ü");
+            Debug.Log("¡°«¡¡ﬂ");
             rb.AddForce(new Vector2(0, (jumpPower + plusJumpPower) * Time.deltaTime), ForceMode2D.Impulse);
             jumpTime += Time.deltaTime;
         }
