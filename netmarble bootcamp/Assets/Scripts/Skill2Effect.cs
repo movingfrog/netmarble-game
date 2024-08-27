d048e8bc6f6276a760cf1ee5ea0e3aa5f4dc0218
icetea1110 <c01042527441@gmail.com>
2024-08-27 22:47:50 +0900

    icetea

--- netmarble bootcamp/Assets/Scripts/Skill2Effect.cs
+++ netmarble bootcamp/Assets/Scripts/Skill2Effect.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Skill2Effect : MonoBehaviour
 {
+    EnemyHealth Damage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("¶§·È´Ù!");
+            Damage = collision.GetComponent<EnemyHealth>();
+            Damage.curHealth -= 45f;
         }
     }
 }
